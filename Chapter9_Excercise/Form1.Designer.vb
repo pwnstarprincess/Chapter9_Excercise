e5d4edd549a5a8dac045a6efaca87f7e52bc64aa
Jennifer Pezzulo <jennifer.pezzulo@gmail.com>
2020-04-18 17:11:27 -0600

    Add project files.

---
+++ Chapter9_Excercise/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.NameBox = New System.Windows.Forms.TextBox()
+        Me.PhoneBox = New System.Windows.Forms.TextBox()
+        Me.MemberListBox = New System.Windows.Forms.ListBox()
+        Me.UpdateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ModifyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AddToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenu = New System.Windows.Forms.MenuStrip()
+        Me.ToolStripMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(138, 387)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(138, 451)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Phone:"
+        '
+        'NameBox
+        '
+        Me.NameBox.Location = New System.Drawing.Point(291, 387)
+        Me.NameBox.Name = "NameBox"
+        Me.NameBox.Size = New System.Drawing.Size(100, 26)
+        Me.NameBox.TabIndex = 2
+        '
+        'PhoneBox
+        '
+        Me.PhoneBox.Location = New System.Drawing.Point(291, 451)
+        Me.PhoneBox.Name = "PhoneBox"
+        Me.PhoneBox.Size = New System.Drawing.Size(100, 26)
+        Me.PhoneBox.TabIndex = 3
+        '
+        'MemberListBox
+        '
+        Me.MemberListBox.FormattingEnabled = True
+        Me.MemberListBox.ItemHeight = 20
+        Me.MemberListBox.Location = New System.Drawing.Point(142, 101)
+        Me.MemberListBox.Name = "MemberListBox"
+        Me.MemberListBox.Size = New System.Drawing.Size(249, 244)
+        Me.MemberListBox.TabIndex = 6
+        '
+        'UpdateToolStripMenuItem
+        '
+        Me.UpdateToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ModifyToolStripMenuItem, Me.AddToolStripMenuItem, Me.DeleteToolStripMenuItem})
+        Me.UpdateToolStripMenuItem.Name = "UpdateToolStripMenuItem"
+        Me.UpdateToolStripMenuItem.Size = New System.Drawing.Size(86, 29)
+        Me.UpdateToolStripMenuItem.Text = "Update"
+        '
+        'ModifyToolStripMenuItem
+        '
+        Me.ModifyToolStripMenuItem.Name = "ModifyToolStripMenuItem"
+        Me.ModifyToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1
+        Me.ModifyToolStripMenuItem.Size = New System.Drawing.Size(202, 34)
+        Me.ModifyToolStripMenuItem.Text = "Modify"
+        '
+        'AddToolStripMenuItem
+        '
+        Me.AddToolStripMenuItem.Name = "AddToolStripMenuItem"
+        Me.AddToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F2
+        Me.AddToolStripMenuItem.Size = New System.Drawing.Size(202, 34)
+        Me.AddToolStripMenuItem.Text = "Add"
+        '
+        'DeleteToolStripMenuItem
+        '
+        Me.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem"
+        Me.DeleteToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F3
+        Me.DeleteToolStripMenuItem.Size = New System.Drawing.Size(202, 34)
+        Me.DeleteToolStripMenuItem.Text = "Delete"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(55, 29)
+        Me.SaveToolStripMenuItem.Text = "Exit"
+        '
+        'ToolStripMenu
+        '
+        Me.ToolStripMenu.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.ToolStripMenu.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.ToolStripMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.UpdateToolStripMenuItem, Me.SaveToolStripMenuItem})
+        Me.ToolStripMenu.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStripMenu.Name = "ToolStripMenu"
+        Me.ToolStripMenu.Size = New System.Drawing.Size(553, 33)
+        Me.ToolStripMenu.TabIndex = 7
+        Me.ToolStripMenu.Text = "MenuStrip1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(553, 553)
+        Me.Controls.Add(Me.ToolStripMenu)
+        Me.Controls.Add(Me.MemberListBox)
+        Me.Controls.Add(Me.PhoneBox)
+        Me.Controls.Add(Me.NameBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ToolStripMenu.ResumeLayout(False)
+        Me.ToolStripMenu.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+        Me.KeyPreview = True
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents NameBox As TextBox
+    Friend WithEvents PhoneBox As TextBox
+    Friend WithEvents MemberListBox As ListBox
+    Friend WithEvents UpdateToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ModifyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AddToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DeleteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenu As MenuStrip
+End Class
---
+++ Chapter9_Excercise/Form1.vb
@@ -0,0 +1,282 @@
+﻿Public Class Form1
+
+    'Create a structure to hold names and phone numbers
+    Structure Membership
+        Dim name As String
+        Dim phone As String
+    End Structure
+
+    'Declare variables
+    Private UpperBound As Integer
+    Private numLines As Integer
+
+    'holds index of name
+    Private nameTextBoxArray As Integer
+    'holds index of phone number
+    Private phoneTextBoxArray As Integer
+    'Initialize structure
+    Private membershipInfo(99) As Membership
+
+    'Variable to hold format for output data
+    Private outputFormat As String = "{0,-20}"
+
+    'Sub routine called at load
+    Private Sub Form_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        'Load and show the membership data
+        LoadMembership()
+        ShowMembership()
+
+    End Sub
+
+    'Sub routine to be called when selecting modify from the form menu
+    Private Sub Modify()
+
+        'variables to hold textbox data
+        Dim newName As String
+        Dim newPhone As String
+        'Get textbox data
+        newName = NameBox.Text
+        newPhone = PhoneBox.Text
+        'update the membership at the selected index with the new data
+        membershipInfo(nameTextBoxArray).name = newName
+        membershipInfo(phoneTextBoxArray).phone = newPhone
+        'Show updated the data in the listbox
+        ShowMembership()
+    End Sub
+
+    'Sub routine to be called when selecting Add from the form menu
+    Private Sub Add()
+
+        'Declare variables to hold textbox data
+        Dim name As String
+        Dim phone As String
+
+        'Get textbox data
+        name = NameBox.Text
+        phone = PhoneBox.Text
+
+        'Increment upper bound
+        UpperBound += 1
+
+        'Add name and phone number to membership structure
+        membershipInfo(UpperBound).name = name
+        membershipInfo(UpperBound).phone = phone
+
+        'Update list box with membership data
+        ShowMembership()
+    End Sub
+
+    'Sub routine to be called when selecting Delete from the form menu
+    Private Sub Delete()
+
+        'Remove selected data from membership data
+        membershipInfo(nameTextBoxArray).name = Nothing
+        membershipInfo(phoneTextBoxArray).phone = Nothing
+
+        'update listbox 
+        ShowMembership()
+    End Sub
+
+    'Sub routine to handle selecting items in listbox
+    Private Sub ListBoxOutput_selectedtext() Handles MemberListBox.SelectedIndexChanged
+
+        '
+        nameTextBoxArray = MemberListBox.SelectedIndex
+        phoneTextBoxArray = MemberListBox.SelectedIndex
+
+        NameBox.Text = MemberListBox.SelectedItem
+        PhoneBox.Text = membershipInfo(phoneTextBoxArray).phone
+
+
+    End Sub
+
+    Sub LoadMembership()
+
+        'Declare streamreader to read membership data
+        Dim readStream As IO.StreamReader
+        'does the file exist?  If so open the txt file
+        If IO.File.Exists("membership.txt") Then
+            readStream = IO.File.OpenText("membership.txt")
+
+            'Look at the incoming characters.  Is there data?
+            'If so ShowMembership
+            If (readStream.Peek <> -1) Then
+                ShowMembership()
+            End If
+            'Read line As long as there is data...
+            'First line is name and second line is number
+            'increment number of lines
+            Do While (readStream.Peek <> -1)
+                Dim unused = readStream.ReadLine()
+                unused = readStream.ReadLine()
+                numLines += 1
+            Loop
+            'Close the stream
+            readStream.Close()
+            'upper bound is one less than the number of lines
+            UpperBound = numLines - 1
+            readStream = IO.File.OpenText("membership.txt")
+
+            'load data into membership info
+            For numberMember As Integer = 0 To UpperBound
+                membershipInfo(numberMember).name = readStream.ReadLine
+                membershipInfo(numberMember).phone = readStream.ReadLine
+            Next
+            'close the stream
+            readStream.Close()
+        Else
+
+            'if the file does not exist prompt the user to create
+            Dim message As String
+            Dim title As String
+            Dim style As MsgBoxStyle
+            Dim response As MsgBoxResult
+
+            message = "membership.txt not found. "
+            message &= " Create file (yes) or close program (no)"
+
+
+            style = MsgBoxStyle.DefaultButton2 Or MsgBoxStyle.Critical Or MsgBoxStyle.YesNo
+            'style = MsgBoxStyle.YesNo
+
+            title = "File Not Found"
+
+            response = MsgBox(message, style, title)
+
+            'Create the file if the user says yes
+            If response = MsgBoxResult.Yes Then
+                Dim sw As IO.StreamWriter = IO.File.CreateText("membership.txt")
+                sw.Close()
+            Else
+                End
+            End If
+        End If
+
+    End Sub
+
+    'Sub routine to show membership names
+    Sub ShowMembership()
+        'Clear the listbox
+        MemberListBox.Items.Clear()
+        'As Long as there are members...
+        For numberMember As Integer = 0 To UpperBound
+            'As long as we have not hit the upper bound
+            For index As Integer = 1 To UpperBound
+                'Sort alphabetically
+                If (membershipInfo(index - 1).name > membershipInfo(index).name) Then
+                    SwapData(index)
+                Else
+                End If
+
+            Next
+        Next
+        'Display the membership data...
+        For numberMember As Integer = 0 To UpperBound
+            MemberListBox.Items.Add(String.Format(outputFormat, membershipInfo(numberMember).name))
+        Next
+
+    End Sub
+
+    'Sub routine to swap data when two values are not in alphabetical order
+    Sub SwapData(ByVal numberMember As Integer)
+        Dim temp As Membership
+        temp = membershipInfo(numberMember - 1)
+        membershipInfo(numberMember - 1) = membershipInfo(numberMember)
+        membershipInfo(numberMember) = temp
+
+    End Sub
+
+    
+    Private Sub AddToolStripMenuItem_Click_1(sender As Object, e As EventArgs) Handles AddToolStripMenuItem.Click
+        'Run add sub routine when menu item clicked
+        Add()
+        'Clear input fields to be ready for next entry
+        NameBox.Clear()
+        PhoneBox.Clear()
+
+    End Sub
+
+    Private Sub ModifyToolStripMenuItem_Click_1(sender As Object, e As EventArgs) Handles ModifyToolStripMenuItem.Click
+        'Run modify subroutine when menu item is clicked
+        Modify()
+    End Sub
+
+    Private Sub DeleteToolStripMenuItem_Click_1(sender As Object, e As EventArgs) Handles DeleteToolStripMenuItem.Click
+        'Run delete subroutine when menu is clicked
+        Delete()
+
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click_1(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+
+        'Prompt to save with msg box
+        Dim message As String
+        Dim title As String
+        Dim style As MsgBoxStyle
+        Dim response As MsgBoxResult
+
+        message = "Save before exiting?"
+        style = MsgBoxStyle.DefaultButton2 Or MsgBoxStyle.Critical Or MsgBoxStyle.YesNo
+
+        title = "Save Before Exiting"
+
+        response = MsgBox(message, style, title)
+
+        If response = MsgBoxResult.Yes Then
+
+            SaveFile()
+            End
+
+        Else
+            End
+
+        End If
+    End Sub
+    'Sub routne to save the file
+    Sub SaveFile()
+
+        'Initialize stream writer
+        Dim sw As IO.StreamWriter = IO.File.CreateText("membership.txt")
+
+        'Until you hit the upepr bound.  If there is data write it.
+        For writeFile As Integer = 0 To UpperBound
+            If membershipInfo(writeFile).name = Nothing Then
+            Else
+                sw.WriteLine(membershipInfo(writeFile).name)
+            End If
+            If membershipInfo(writeFile).phone = Nothing Then
+            Else
+                sw.WriteLine(membershipInfo(writeFile).phone)
+            End If
+        Next
+        sw.Close()
+
+    End Sub
+
+    Protected Overrides Function ProcessCmdKey(ByRef msg As Message, ByVal keyData As Keys) As Boolean
+        'Handle shortcut keys
+        Select Case keyData
+            Case Keys.F1
+                Modify()
+                Return True
+            Case Keys.F2
+                Add()
+                Return True
+            Case Keys.F3
+                Delete()
+                Return True
+            Case Else
+                Exit Select
+        End Select
+
+        Return False
+
+    End Function
+
+End Class
+
+
+
+
+
